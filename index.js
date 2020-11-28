de569d430bf7966a445ef0eff66b6aea53e3e2e0
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-11-28 02:43:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
+  
+  static capitalize(string) {
+    return string.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ")
+  }
+
+  static sanitize(string) {
+    return string.split(" ").map(word => word.replace(/[^A-Za-z0-9-']+/g, '')).join(" ")
+  }
+
+  static titleize(string) {
+    let avoidable = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let result = []
+    let arrayOfString = string.split(" ")
+    for (let i=0; i<arrayOfString.length; i++){
+      let currentEl = arrayOfString[i]
+      if (i===0) result.push(this.capitalize(currentEl))
+      else if (avoidable.includes(currentEl)) result.push(currentEl)
+      else result.push(this.capitalize(currentEl))
+    }
+    return result.join(" ")
+  }
+
 }
